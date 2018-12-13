7e15cdf05558d35cf7274fb1342802b3473ac3b8
luoxiaojun <luoxiaojun@dodoca.com>
2018-12-13 10:35:38 +0800

    remove useless config

--- src/config/elasticsearch.php
+++ src/config/elasticsearch.php
@@ -1,15 +1,11 @@
 <?php
 
-use Monolog\Logger;
-
 return [
     'connections' => [
         'default' => [
             'hosts' => [
                 env('ES_SERVER', ''),
             ],
-            'logPath' => env('ES_LOG_PATH', ''),
-            'logLevel' => env('ES_LOG_LEVEL', Logger::INFO),
         ],
     ],
 ];
